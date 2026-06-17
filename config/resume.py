729f41be8e350f58ff3ab38f2c31a8d18a81ada3
Zafran <zafranzafran0234@gmail.com>
2026-06-18 00:06:08 +0800

    chore: stripped old license headers from config files
    removed all the 'gnu affero general public license' junk from
    the tops of every config file. they were left over from the
    original project and dont belong here anymore.

--- config/resume.py
+++ config/resume.py
@@ -1,15 +1,10 @@
 '''
-
-License:    GNU Affero General Public License
-            https://www.gnu.org/licenses/agpl-3.0.en.html
-
 '''
 
 from personals import *
 import json
 
 ###################################################### CONFIGURE YOUR RESUME HERE ######################################################
-
 
 # # Give an relative path of your default resume to be uploaded. If file in not found, will continue using your previously uploaded resume in LinkedIn.
 # default_resume_path = "all resumes/default/resume.pdf"      # (In Development)
@@ -18,20 +13,9 @@ import json
 YOU DON'T HAVE TO EDIT THIS FILE, IF YOU ADDED YOUR DEFAULT RESUME.
 '''
 
-
 # resume_headline = json({
 #     "first_name": first_name,
 # })
-
-
-
-
-
-
-
-
-
-
 
 # # >>>>>>>>>>> RELATED SETTINGS <<<<<<<<<<<
 
